84711bcb86768862c011b193399af0a605c9c3eb
koemsakmean <koemsak.mean@gmail.com>
2026-02-26 16:22:41 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calendar-lunar',
+  webDir: 'www'
+};
+
+export default config;
