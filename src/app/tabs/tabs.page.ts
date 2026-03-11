d3d67a5e7c649a07140c77a3f60482018f4ac34a
koemsakmean <koemsak.mean@gmail.com>
2026-03-11 16:32:42 +0700

    Update tab menu

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,7 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { Component, ViewChild, EnvironmentInjector, inject } from '@angular/core';
+import { IonTabs, IonTabBar, IonIcon } from '@ionic/angular/standalone';
+import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { alarmOutline, calendarOutline, listOutline } from 'ionicons/icons';
 
@@ -7,12 +9,22 @@ import { alarmOutline, calendarOutline, listOutline } from 'ionicons/icons';
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  imports: [IonTabs, IonTabBar, IonIcon, NgClass],
 })
 export class TabsPage {
+  @ViewChild('tabs') tabs!: IonTabs;
+
+  activeTab = 'tab1';
   public environmentInjector = inject(EnvironmentInjector);
+  private router = inject(Router);
 
   constructor() {
     addIcons({ calendarOutline, listOutline, alarmOutline });
+  }
+
+  switchTab(tab: string) {
+    this.activeTab = tab;
+    this.router.navigate([`/tabs/${tab}`]);
+    if (window.navigator.vibrate) window.navigator.vibrate(10);
   }
 }
